79d57ea49f3a5cd76ca12fc3f70c6af2fe5e8119
unknown <luccasdrapala@gmail.com>
2023-02-28 11:24:27 -0300

    varias fita

--- php-operators.php
+++ php-operators.php
@@ -18,3 +18,64 @@ echo '<br>';
 // multipicacao
 echo 'Multiplicacao: ' . $number * $otherNumber;
 echo '<br>';
+
+//concatenacao
+echo 'Numero 1: ' . $number . ' <br>Numero 2: ' . $otherNumber;
+echo '<br>';
+
+// atribuição
+$number += 1;  
+echo '$number += 1: ' . $number;
+echo '<br>';
+
+$number -= 5;
+echo '$number -= 5: ' . $number;
+echo '<br>';
+
+$number *= 2;
+echo '$number *= 2: ' . $number;
+echo '<br>';
+
+$number /= 3;
+echo '$number /= 3: ' . $number;
+echo '<br>';
+
+$number %= 3;
+echo '$number %= 3: ' . $number;
+
+// <, >, <=, >=, !=, ==, ===
+echo '<hr>';
+echo '$number: ' . $number;
+echo '<br>';
+echo '$otherNumber: ' . $otherNumber;
+echo '<br><br>';
+
+echo '$number > $otherNumber: '; 
+$result = $number > $otherNumber;
+var_dump($result);
+echo '<hr>';
+
+echo '$number < $otherNumber: '; 
+$result = $number < $otherNumber;
+var_dump($result);
+echo '<hr>';
+
+echo '"5" != $otherNumber: '; 
+$result = '5'!= $otherNumber;
+var_dump($result);
+echo '<hr>';
+
+echo ' "5" !== $otherNumber: '; 
+$result = '5' !== $otherNumber;
+var_dump($result);
+echo '<hr>';
+
+echo ' "5" == $otherNumber: '; 
+$result = '5' == $otherNumber;
+var_dump($result);
+echo '<hr>';
+
+echo ' "5" === $otherNumber: '; 
+$result = '5' === $otherNumber;
+var_dump($result);
+echo '<hr>';
