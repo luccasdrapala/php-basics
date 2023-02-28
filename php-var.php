bc8d1300d930926cfb98771e8a6f7cc25c32e7fc
unknown <luccasdrapala@gmail.com>
2023-02-28 09:43:18 -0300

    add files

---
+++ php-var.php
@@ -0,0 +1,27 @@
+<?php 
+
+//variaveis names
+// camelCase
+// snake_case
+
+//strings
+$exString = 'Isso é uma string';
+
+//number: integer, float(double)
+$exNum = 666;
+
+//booleans
+$exBol = true;
+
+//arrays
+$exArr = ['valor', 'Outro', 12];
+
+//object
+$exObj = [
+    'a' => 'Valor 1',
+    'b' => 'Valor 2'
+];
+
+//null
+$exNum = null;
+    
